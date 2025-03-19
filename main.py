179fa3f8509ff64e152c0abf699ddc9c197f8773
loki <loki266@yahoo.com>
2025-03-19 16:30:24 +1100

    n update

---
+++ main.py
@@ -0,0 +1,11 @@
+size = 2
+waist = 45
+
+if size < 10 and waist < 50:
+    print("good job!")
+
+elif size < 15 or waist > 50:
+    print("I do not know what I'm taking about")
+
+else:
+    print("this is hardware shop!")
