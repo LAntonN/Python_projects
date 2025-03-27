e4b58d80971cfad73f7b3f8f6bd4284d54423f85
loki <loki266@yahoo.com>
2025-03-27 14:02:20 +1100

    map test

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+class Student:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+students= [Student("Joe", 0.46), Student("Amy", 0.72), Student("Mark", 0.88), Student("Zach", 0.75), Student("Jane", 0.84), Student("Sara", 0.38)] 
+
+student_results = []
+for student in students:
+  #  if student.score >= 0.7:
+   #     student_results.append(f"{student.name} Passed")
+  #  else:
+   #     student_results.append(f"{student.name} Failed")
+    
+    student_results.append(f"{student.name} Passed") if student.score >= 0.7 else student_results.append(f"{student.name} Failed")
+
+map_results = list(map(lambda student: student.name, students))
+
+
+print(map_results)
+
+
+numbers = [1,2,3,4,5]
+map_multiplier = list(map(lambda number: number * 2, numbers))
+
+
+print(map_multiplier)
+
+
+
+
+
+
+
+
+
+
 #size = 2
 #waist = 45
 
